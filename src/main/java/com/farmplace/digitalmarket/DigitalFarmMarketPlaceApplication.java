ab1a1802c00befccf16fbee0990b8cb42085745c
James Okutoyi <okutoyijemo@gmail.com>
2025-10-09 11:45:00 +0300

    Projection creation with intellij idea and spring initializr

---
+++ src/main/java/com/farmplace/digitalmarket/DigitalFarmMarketPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.farmplace.digitalmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalFarmMarketPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalFarmMarketPlaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmplace/digitalmarket/DigitalFarmMarketPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmplace.digitalmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalFarmMarketPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
